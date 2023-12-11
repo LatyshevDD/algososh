d23a7c0de0bc054ea9722924ba26cac8a1113210
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-12-11 20:59:21 +0300

    feat: добавить лоадер и блокировку интерфейса на время работы алгоритма списка

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -20,6 +20,13 @@ export const ListPage: React.FC = () => {
   const[modifiedState, setModifiedState] = useState(-1)
   const[changingState, setChangingState] = useState(-1)
   const[tailNode, setTailNode] = useState(-1)
+  const[addTailLoader, setAddTailLoader] = useState(false)
+  const[addHeadLoader, setAddHeadLoader] = useState(false)
+  const[deleteHeadLoader, setDeleteHeadLoader] = useState(false)
+  const[deleteTailLoader, setDeleteTailLoader] = useState(false)
+  const[addByIndexLoader, setAddByIndexLoader] = useState(false)
+  const[deleteByIndexLoader, setDeleteByIndexLoader] = useState(false)
+  const[disableInterface, setDisableInterface] = useState(false)
 
   const linkedList = new LinkedList(array)
 
@@ -34,6 +41,8 @@ export const ListPage: React.FC = () => {
     if(string.length === 0) {
       return
     }
+    setAddTailLoader(true)
+    setDisableInterface(true)
     setHeadNode(linkedList.getSize() - 1)
     await pause(500)
     setString('')
@@ -43,12 +52,16 @@ export const ListPage: React.FC = () => {
     setModifiedState(linkedList.getSize() - 1)
     await pause(500)
     setModifiedState(-1)
+    setAddTailLoader(false)
+    setDisableInterface(false)
   }
 
   const handleAddHead = async() => {
     if(string.length === 0) {
       return
     }
+    setAddHeadLoader(true)
+    setDisableInterface(true)
     setHeadNode(0)
     await pause(500)
     setString('')
@@ -58,28 +71,40 @@ export const ListPage: React.FC = () => {
     setModifiedState(0)
     await pause(500)
     setModifiedState(-1)
+    setAddHeadLoader(false)
+    setDisableInterface(false)
   }
 
   const handleDeleteHead = async() => {
+    setDeleteHeadLoader(true)
+    setDisableInterface(true)
     setTailNode(0)
     await pause(500)
     setTailNode(-1)
     linkedList.deleteHead()
     setArray(linkedList.toArray())
+    setDeleteHeadLoader(false)
+    setDisableInterface(false)
   }
 
   const handleDeleteTail = async() => {
+    setDeleteTailLoader(true)
+    setDisableInterface(true)
     setTailNode(linkedList.getSize() - 1)
     await pause(500)
     setTailNode(-1)
     linkedList.deleteTail()
     setArray(linkedList.toArray())
+    setDeleteTailLoader(false)
+    setDisableInterface(false)
   }
 
   const handleAddByIndex = async() => {
     if(string.length === 0 || index < 0) {
       return
     }
+    setAddByIndexLoader(true)
+    setDisableInterface(true)
     let currentIndex = -1
     while(currentIndex <= index) {
       setHeadNode(currentIndex)
@@ -96,12 +121,16 @@ export const ListPage: React.FC = () => {
     setModifiedState(-1)
     setString('')
     setIndex(-1)
+    setAddByIndexLoader(false)
+    setDisableInterface(false)
   }
 
   const handleDeleteByIndex = async() => {
     if(index < 0) {
       return
     }
+    setDeleteByIndexLoader(true)
+    setDisableInterface(true)
     let currentIndex = -1
     while(currentIndex <= index) {
       setChangingState(currentIndex)
@@ -115,6 +144,8 @@ export const ListPage: React.FC = () => {
     linkedList.deleteByIndex(index)
     setArray(linkedList.toArray())
     setIndex(-1)
+    setDeleteByIndexLoader(false)
+    setDisableInterface(false)
   }
 
   return (
@@ -130,26 +161,35 @@ export const ListPage: React.FC = () => {
               isLimitText = {true}
               extraClass={styles.text_input}
               onChange={(e) => setString(e.currentTarget.value)}
+              disabled={disableInterface}
             />
             <Button 
               type='button' 
               text='Добавить в head'
               onClick={handleAddHead}
+              isLoader={addHeadLoader}
+              disabled={disableInterface}
             />
             <Button 
               type='button' 
               text='Добавить в tail'
               onClick={handleAddTail}
+              isLoader={addTailLoader}
+              disabled={disableInterface}
             />
             <Button 
               type='button' 
               text='Удалить из head'
               onClick={handleDeleteHead}
+              isLoader={deleteHeadLoader}
+              disabled={disableInterface}
             />
             <Button 
               type='button' 
               text='Удалить из tail'
               onClick={handleDeleteTail}
+              isLoader={deleteTailLoader}
+              disabled={disableInterface}
             />
           </div>
           <div className={styles.input_container}>
@@ -160,18 +200,23 @@ export const ListPage: React.FC = () => {
               maxLength = {1}
               extraClass={styles.text_input}
               onChange={(e) => setIndex(parseInt(e.currentTarget.value))}
+              disabled={disableInterface}
             />
             <Button 
               type='button' 
               text='Добавить по индексу'
               extraClass={styles.flex_grow}
               onClick={handleAddByIndex}
+              isLoader={addByIndexLoader}
+              disabled={disableInterface}
             />
             <Button 
               type='button' 
               text='Удалить по индексу'
               extraClass={styles.flex_grow}
               onClick={handleDeleteByIndex}
+              isLoader={deleteByIndexLoader}
+              disabled={disableInterface}
             />
           </div>
           <div className={styles.circles}>
