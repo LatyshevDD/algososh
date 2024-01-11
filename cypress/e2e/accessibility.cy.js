8fcd969dc01297de969d5a59d3a547612bbcd1cd
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-01-11 16:27:37 +0300

    feat: реализовать e2e тест роутинга приложения

---
+++ cypress/e2e/accessibility.cy.js
@@ -0,0 +1,5 @@
+describe('accessibility spec', () => {
+  it('accessible', () => {
+    cy.visit('http://localhost:3000/')
+  })
+})
---
+++ cypress/e2e/routing.cy.js
@@ -0,0 +1,37 @@
+describe('routing spec', () => {
+  
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('string page routing test', () => {
+    cy.visit('http://localhost:3000/recursion')
+    cy.contains('Строка')
+  })
+
+  it('fibonacci page routing test', () => {
+    cy.visit('http://localhost:3000/fibonacci')
+    cy.contains('Фибоначчи')
+  })
+
+  it('sorting page routing test', () => {
+    cy.visit('http://localhost:3000/sorting')
+    cy.contains('Сортировка массива')
+  })
+
+  it('stack page routing test', () => {
+    cy.visit('http://localhost:3000/stack')
+    cy.contains('Стек')
+  })
+
+  it('queue page routing test', () => {
+    cy.visit('http://localhost:3000/queue')
+    cy.contains('Очередь')
+  })
+
+  it('list page routing test', () => {
+    cy.visit('http://localhost:3000/list')
+    cy.contains('Связный список')
+  })
+
+})
